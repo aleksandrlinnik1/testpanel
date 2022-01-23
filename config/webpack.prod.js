47be62e4bbf73e133d6cb1dc07f6fb9b16d01f61
Aleksandr Linnik <aleksandrlinnik1@gmail.com>
2022-01-23 21:59:35 +0300

    fix webpack config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
   devtool: false,
   output: {
     path: paths.build,
-    publicPath: '/',
+    publicPath: '/testpanel/dist',
     filename: 'js/[name].[contenthash].bundle.js',
   },
   module: {
